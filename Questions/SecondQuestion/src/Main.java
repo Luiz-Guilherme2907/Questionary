258232d66d17c5ed083b7aa7caa35a4ba3b7becd
Luiz-Guilherme2907 <luizguilhermemodler2907@gmail.com>
2024-03-07 16:31:14 -0300

    Implemented the Fibonacci sequence

--- Questions/SecondQuestion/src/Main.java
+++ Questions/SecondQuestion/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         // Definindo o número limite da sequência de Fibonacci
         int limite = 100;
 
-        // Calculando e armazenando os valores da sequência de Fibonacci até o limite
         int a = 0;
         int b = 1;
         int c = 0;
@@ -16,8 +15,6 @@ public class Main {
             a = b;
             b = c;
         }
-
-        // Verificando se um número pertence à sequência de Fibonacci
         Scanner scanner = new Scanner(System.in);
         System.out.print("Informe um número para verificar se pertence à sequência de Fibonacci: ");
         int numero = scanner.nextInt();
